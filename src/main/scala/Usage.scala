001091b24ae33221f2856e947eb60f344b9bf3f6
David Peklak <dpeklak@gmail.com>
2014-12-14 10:20:48 +0100

    make StateEffect work with List

--- src/main/scala/Usage.scala
+++ src/main/scala/Usage.scala
@@ -15,8 +15,9 @@ object Usage {
 
     val stateProgram: FreeCT[Unit] =
       for {
-        i <- fGet
-        _ <- fPut(i * 3)
+        i <- 3 : FreeCT [Int]
+        j <- fGet
+        _ <- fPut(i * j)
       } yield ()
 
     //// Interpretation
@@ -27,7 +28,33 @@ object Usage {
 
     // run with Usage.StateEffectId.stateRun(3)
     lazy val stateRun = Free.runFC[StateEffect[Id, Int]#F, interpret.StateTS, Unit](stateProgram)(interpret.transToState)
+  }
 
+  object StateEffectList {
+
+    val MMList = implicitly[Monad[List]] // I have to initialize that before using stateRun below
+
+    val idStateEffect = new StateEffect[List, Int] {
+      implicit def MM: Monad[List] = MMList
+    }
+
+    import idStateEffect._
+
+    val stateProgram: FreeCT[Unit] =
+      for {
+        i <- List(1, 2, 3) : FreeCT[Int]
+        j <- fGet
+        _ <- fPut(i * j)
+      } yield ()
+
+    //// Interpretation
+
+    val interpret = new StateEffectInterpret[List, Int] {
+      implicit def MM: Monad[List] = MMList
+    }
+
+    // run with Usage.StateEffectList.stateRun(3)
+    lazy val stateRun = Free.runFC[StateEffect[List, Int]#F, interpret.StateTS, Unit](stateProgram)(interpret.transToState)
   }
 
 }
